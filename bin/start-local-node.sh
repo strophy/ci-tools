5653fb180419f4fef19dee90f1388e67d417d647
Ivan Shumkov <ivan@shumkov.ru>
2020-10-28 22:22:57 +0300

    feat: set DPNS contract height variable (#8)

--- bin/start-local-node.sh
+++ bin/start-local-node.sh
@@ -46,18 +46,20 @@ case ${i} in
 esac
 done
 
-#Define variables
-echo "Defining versions to download"
+# Define variables
+
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CURRENT_VERSION=$("$DIR"/get-release-version "$PACKAGE_JSON_PATH" "$major_version")
 MN_RELEASE_LINK=$("$DIR"/get-github-release-link "$PACKAGE_JSON_PATH" dashevo/mn-bootstrap "$major_version" "$minor_version")
 
-#Create temp dir
+echo "Current version: ${CURRENT_VERSION}";
+
+# Create temp dir
 TMP="$DIR"/../tmp
 rm -rf "$TMP"
 mkdir "$TMP"
 
-#Download dapi from defined branch
+# Download dapi from defined branch
 mn_bootstrap_dapi_options="--dapi-image-build-path="
 if [ -n "$dapi_branch" ]
 then
@@ -69,7 +71,7 @@ then
   mn_bootstrap_dapi_options="--dapi-image-build-path=$TMP/dapi"
 fi
 
-#Download drive from defined branch
+# Download drive from defined branch
 mn_bootstrap_drive_options="--drive-image-build-path="
 if [ -n "$drive_branch" ]
 then
@@ -81,7 +83,7 @@ then
   mn_bootstrap_drive_options="--drive-image-build-path=$TMP/drive"
 fi
 
-#Download and install mn-bootstrap
+# Download and install mn-bootstrap
 echo "Installing mn-bootstrap"
 curl -L "$MN_RELEASE_LINK" > "$TMP"/mn-bootstrap.tar.gz
 mkdir "$TMP"/mn-bootstrap && tar -C "$TMP"/mn-bootstrap -xvf "$TMP"/mn-bootstrap.tar.gz
@@ -89,19 +91,31 @@ MN_RELEASE_DIR="$(ls "$TMP"/mn-bootstrap)"
 cd "$TMP"/mn-bootstrap/"$MN_RELEASE_DIR"
 npm ci && npm link
 
-#Initialize mn-bootstrap
-echo "Initializing mn-bootstrap"
+# Setup node for local node mn-bootstrap
+echo "Setting up a local node"
 
 mn config:default local
 mn config:set core.miner.enable true
 mn config:set core.miner.interval 1s
+mn config:set environment development
+mn config:set platform.drive.abci.log.level debug
 
 OUTPUT=$(mn setup-for-local-development "$mn_bootstrap_dapi_options" "$mn_bootstrap_drive_options")
 
 FAUCET_PRIVATE_KEY=$(echo "$OUTPUT" | grep -m 1 "Private key:" | awk '{printf $4}')
-DPNS_CONTRACT_ID=$(mn config:get platform.dpns.contractId)
+DPNS_CONTRACT_ID=$(mn config:get platform.dpns.contract.id)
+DPNS_CONTRACT_BLOCK_HEIGHT=$(mn config:get platform.dpns.contract.height)
 DPNS_TOP_LEVEL_IDENTITY_ID=$(mn config:get platform.dpns.ownerId)
 DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY=$(echo "$OUTPUT" | grep -m 1 "HD private key:" | awk '{$1=""; printf $5}')
+
+echo "Node is configured:"
+
+echo "FAUCET_PRIVATE_KEY: ${FAUCET_PRIVATE_KEY}"
+echo "DPNS_CONTRACT_ID: ${DPNS_CONTRACT_ID}"
+echo "DPNS_CONTRACT_BLOCK_HEIGHT: ${DPNS_CONTRACT_BLOCK_HEIGHT}"
+echo "DPNS_TOP_LEVEL_IDENTITY_ID: ${DPNS_TOP_LEVEL_IDENTITY_ID}"
+echo "DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY: ${DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY}"
+
 
 #Start mn-bootstrap
 echo "Starting mn-bootstrap"
@@ -113,5 +127,6 @@ export FAUCET_PRIVATE_KEY
 export DPNS_TOP_LEVEL_IDENTITY_PRIVATE_KEY
 export DPNS_TOP_LEVEL_IDENTITY_ID
 export DPNS_CONTRACT_ID
+export DPNS_CONTRACT_BLOCK_HEIGHT
 
 echo "Success"
